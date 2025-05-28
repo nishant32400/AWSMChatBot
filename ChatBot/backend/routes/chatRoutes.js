da0c15a7b57784f564168f7773291bbd4c46a45f
nishant32400 <67227861+nishant32400@users.noreply.github.com>
2025-05-28 09:29:05 +0530

    Update chatRoutes.js

--- ChatBot/backend/routes/chatRoutes.js
+++ ChatBot/backend/routes/chatRoutes.js
@@ -1,4 +1,4 @@
-// backend/routes/chatRoutes.js
+
 const express = require("express");
 const router = express.Router();
 const { chatWithBot, getChatHistory } = require("../controllers/chatController");
